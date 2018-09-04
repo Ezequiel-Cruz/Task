be40511db10af9218c9c36e8b461d3a1c750b926
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2018-09-04 10:36:19 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAXHbsTnPHFpSy4HxvXbxpErJaWA5aSsYc',
-    authDomain: 'tasks-7c30e.firebaseapp.com',
-    databaseURL: 'https://tasks-7c30e.firebaseio.com',
-    projectId: 'tasks-7c30e',
-    storageBucket: 'tasks-7c30e.appspot.com',
-    messagingSenderId: '245649376549'
+   
   }
 };
